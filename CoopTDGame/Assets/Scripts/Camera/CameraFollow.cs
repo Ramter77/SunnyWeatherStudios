e7474273abde44d27d868c79eef0b5f9ab91ee12
JannisLauer <40301636+JannisLauer@users.noreply.github.com>
2019-03-18 23:12:37 +0100

    Changed the Camera in Game Scene

---
+++ CoopTDGame/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float cameraMoveSpeed = 120f;
+    public GameObject cameraFollowObject;
+    private Vector3 followPos;
+    public float clampAngle = 80f;
+    public float inputSensivity = 150f;
+    public GameObject cameraObj;
+    public GameObject PlayerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    private float rotX = 0.0f;
+    private float rotY = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    private void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        Transform target = cameraFollowObject.transform;
+
+        float step = cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
