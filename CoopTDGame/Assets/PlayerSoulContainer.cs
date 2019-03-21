a2800803a9f9aa4acc526372cba7676aeb2c0509
JannisLauer <40301636+JannisLauer@users.noreply.github.com>
2019-03-21 17:16:40 +0100

    added player soul container

---
+++ CoopTDGame/Assets/PlayerSoulContainer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerSoulContainer : MonoBehaviour
+{
+
+    [Header("Player shared Soul storage")]
+    [Tooltip("current shared souls")]
+    public int playerSoulCount;
+    public Text playerSoulDountDisplayText;
+
+
+    [Tooltip("Amount that the players pays for using ability 1")]
+    public int Ability1Cost = 0;
+    public int Ability2Cost = 0;
+    public int Ability3Cost = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
