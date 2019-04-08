2f72f649f8aad4ccac0da6981f75442849800e5c
JannisLauer <40301636+JannisLauer@users.noreply.github.com>
2019-04-08 04:50:18 +0200

    Bunch of prefab and script changes

--- CoopTDGame/Assets/Scripts/Player/PlayerWeaponDamage.cs
+++ CoopTDGame/Assets/Scripts/Player/PlayerWeaponDamage.cs
@@ -5,26 +5,18 @@ using UnityEngine;
 public class PlayerWeaponDamage : MonoBehaviour
 {
     public float attackDamage = 0f;
-
-    public GameObject ParentEnemy;
-    // Start is called before the first frame update
+    
     void Start()
     {
-        if (ParentEnemy != null) {
-            attackDamage = ParentEnemy.GetComponent<MeleeAttack>().attackDamage;
-        }
-        else {
-            //Debug.Log("ParentEnemy is null: " + gameObject.name);
-            attackDamage = 40;  //default to 40 dmg to test
-        }
+        
         
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Enemy" )
+        if(other.gameObject.tag == "Enemy")
         {
-            //Debug.Log("Weapon hit enemy");
+            Debug.Log("Weapon hit enemy");
             other.gameObject.GetComponent<LifeAndStats>().TakeDamage(attackDamage); //!public functions pls
             //Destroy(gameObject);
         }
