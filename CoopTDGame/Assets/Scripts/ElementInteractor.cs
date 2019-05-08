901b7887313fc5c957e5ece6b17dfb64bad4875d
JannisLauer <40301636+JannisLauer@users.noreply.github.com>
2019-05-08 18:13:13 +0200

    Interaction between elements

---
+++ CoopTDGame/Assets/Scripts/ElementInteractor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementInteractor : MonoBehaviour
+{
+    [Header("Element - Settings")]
+    public Element elementType;
+
+    [Tooltip("Tag used for the projectiles of players")]
+    [SerializeField] private string projectileTag = "ElementProjectile";
+
+    [Header("Interaction Settings")]
+    [Tooltip("indicates if the prjectile is ready to interact with another one")]
+    public bool allowInteraction = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == projectileTag)
+        {
+            if (other.GetComponent<ElementInteractor>())
+            {
+                if(allowInteraction)
+                {
+                    Element otherProjectileElement;
+                    otherProjectileElement = other.GetComponent<ElementInteractor>().elementType;
+                    Debug.Log(other.gameObject.name + other.GetComponent<ElementInteractor>().elementType);
+                    if (otherProjectileElement != elementType)
+                    {
+                        ElementManager.Instance.requestingInteractor = this.gameObject;
+                        ElementManager.Instance.combineElement(elementType, otherProjectileElement);
+                        allowInteraction = false;
+                    }
+                }
+            }
+        }
+    }
+
+    public void createReaction(GameObject reactionPrefab)
+    {
+        Instantiate(reactionPrefab, transform.position, Quaternion.identity);
+    }
+
+}
---
+++ CoopTDGame/Assets/Scripts/ElementManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Element { NoElement, Fire, Ice };
+public enum CombinationElement { Blast };
+public class ElementManager : Singleton<ElementManager>
+{
+    [Header("Reaction Settings")]
+    [Tooltip("List of all effects that can be created by combining elements")]
+    public GameObject[] reactionPrefabs;
+
+    [Tooltip("the GameObject that triggers the interaction between elements")]
+    public GameObject requestingInteractor = null;
+
+    [Tooltip("Product of the reaction")]
+    public CombinationElement productElement;
+
+    [Tooltip("used to pick the corrosponding reaction element")]
+    [SerializeField] private int reactionIndex = 0;
+
+    public void combineElement(Element firstElement, Element secondElement)
+    {
+        if ((firstElement == Element.Fire && secondElement == Element.Ice) || (firstElement == Element.Ice && secondElement == Element.Fire))
+        {
+            productElement = CombinationElement.Blast;
+            reactionIndex = 0;
+            startReaction();
+        }
+    }
+
+    void startReaction()
+    {
+        if(requestingInteractor != null)
+        {
+            GameObject effectPrefab;
+            effectPrefab = reactionPrefabs[reactionIndex];
+            requestingInteractor.GetComponent<ElementInteractor>().createReaction(effectPrefab);
+            requestingInteractor = null;
+        }
+    }  
+}
