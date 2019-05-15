c8f45037691771eec03214e3587e9cd5a419bc72
JannisLauer <40301636+JannisLauer@users.noreply.github.com>
2019-05-15 23:24:03 +0200

    Camera Reacting to terrain, small movement changes

--- CoopTDGame/Assets/Scripts/Camera/CameraCollision.cs
+++ CoopTDGame/Assets/Scripts/Camera/CameraCollision.cs
@@ -8,10 +8,11 @@ public class CameraCollision : MonoBehaviour
     public float maxDistance = 4f;
     public float smooth = 10f;
 
-    Vector3 dollyDir;
+    public Vector3 dollyDir;
     public Vector3 dollyDirAdjusted;
     public float distance;
 
+    public bool isHitting = false;
 
     void Awake()
     {
@@ -22,13 +23,12 @@ public class CameraCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
         RaycastHit hit;
-
         if(Physics.Linecast(transform.parent.position, desiredCameraPos, out hit))
         {
-            distance = Mathf.Clamp((hit.distance * 0.7f), minDistance, maxDistance);
-
+            distance = Mathf.Clamp((hit.distance * 0.9f), minDistance, maxDistance);
+            isHitting = true;
         }
         else
         {
@@ -37,4 +37,5 @@ public class CameraCollision : MonoBehaviour
         }
         transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
     }
+    
 }
