efef2f0132c9dbb73a1ecebf4abbe82c1c2b81b3
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2019-05-28 18:02:52 +0200

    Fixes & added soul sphere sway
    Fixed enemy projectile colliding with attack point triggered collider

---
+++ CoopTDGame/Assets/Scripts/Utility/LoopAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private bool playAnimationOnStart;
+    private Animation anim;
+    
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+
+        if (playAnimationOnStart) {
+            anim.Play();
+        }
+    }
+}
