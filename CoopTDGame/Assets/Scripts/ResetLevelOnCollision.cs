70dfe8006872316c2694cd5a0a7f90b826d1a001
JannisLauer <40301636+JannisLauer@users.noreply.github.com>
2019-05-29 00:43:59 +0200

    Reset Collider under Map / Added Colliders for Soul Sphere

---
+++ CoopTDGame/Assets/Scripts/ResetLevelOnCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevelOnCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" || other.gameObject.tag == "Player2")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
