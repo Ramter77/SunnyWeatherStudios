5c0bb97da6d050a97a4fae071135e7bb9bd8204b
JannisLauer <40301636+JannisLauer@users.noreply.github.com>
2019-06-01 00:11:52 +0200

    Implemented PH Main Menu

---
+++ CoopTDGame/Assets/GraphicSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicSettings : MonoBehaviour
+{
+    /// <summary>
+    /// Low, Medium, High, Ultra
+    /// </summary>
+    /// <param name="qualityIndex"></param>
+    public void setQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
