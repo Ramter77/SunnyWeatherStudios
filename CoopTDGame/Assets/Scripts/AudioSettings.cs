a55037351153e37eabd5e5105be58118617f33bb
JannisLauer <40301636+JannisLauer@users.noreply.github.com>
2019-05-31 23:11:04 +0200

    Sound Settings
    Added multiple sliders to set volumes for master, music and sound effects.

---
+++ CoopTDGame/Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSettings : MonoBehaviour
+{
+    public AudioMixer mainMixer;
+
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("volume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        mainMixer.SetFloat("musicVol", volume);
+    }
+
+    public void setEffectVolume(float volume)
+    {
+        mainMixer.SetFloat("effectVol", volume);
+    }
+}
