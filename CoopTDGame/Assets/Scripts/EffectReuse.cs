93e5eaea2df07fa2fdfbc3f4f4adc48027ea6c31
JannisLauer <40301636+JannisLauer@users.noreply.github.com>
2019-06-01 22:49:01 +0200

    Explosion Sounds - needs fixing

--- CoopTDGame/Assets/Scripts/EffectReuse.cs
+++ CoopTDGame/Assets/Scripts/EffectReuse.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.MultiAudioListener;
 
 public class EffectReuse : PoolObject
 {
     ParticleSystem myParticleSystem;
+    [Tooltip("0 = normal; 1 = Fire; 2 = Ice; 3 = Blast")] public int explosionIndex;
+    private MultiAudioSource _source;
 
     private void Start()
     {
         myParticleSystem = GetComponent<ParticleSystem>();
+        _source = GetComponent<MultiAudioSource>();
     }
 
     public override void OnObjectReuse()
@@ -19,6 +23,15 @@ public class EffectReuse : PoolObject
             myParticleSystem.Play();
             //Invoke("HideSelf", 2);
         }
+        if(explosionIndex == 0)
+            AudioManager.Instance.PlaySound(_source, Sound.towerDefaultImpact);
+        if (explosionIndex == 1)
+            AudioManager.Instance.PlaySound(_source, Sound.towerFireImpact);
+        if (explosionIndex == 2)
+            AudioManager.Instance.PlaySound(_source, Sound.towerIceImpact);
+        if (explosionIndex == 3)
+            AudioManager.Instance.PlaySound(_source, Sound.towerBlastImpact);
+
     }
 
     void HideSelf()
