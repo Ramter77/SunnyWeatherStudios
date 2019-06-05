c41c55d429624977eeff1a560f22528a80ed9237
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2019-06-05 05:59:28 +0200

    Escape to menu

--- CoopTDGame/Assets/Scripts/Utility/GameManager.cs
+++ CoopTDGame/Assets/Scripts/Utility/GameManager.cs
@@ -4,6 +4,42 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    public GameObject UIcanvas;
+    public GameObject MainMenu;
+    public GameObject menuCam;
+    public GameObject player1, player2;
+    private bool menuEnabled;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (menuEnabled) {
+                menuEnabled = false;
+                UIcanvas.SetActive(false);
+                MainMenu.SetActive(false);
+                menuCam.SetActive(false);
+                player1.SetActive(true);
+                player2.SetActive(true);
+
+                InputManager.Instance._MouseControl.LockMouse = false;
+                InputManager.Instance._MouseControl.hideCursor = false;
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                menuEnabled = true;
+                UIcanvas.SetActive(true);
+                MainMenu.SetActive(true);
+                menuCam.SetActive(true);
+                player1.SetActive(false);
+                player2.SetActive(false);
+
+                InputManager.Instance._MouseControl.LockMouse = true;
+                InputManager.Instance._MouseControl.hideCursor = true;
+                Time.timeScale = 0.01f;
+            }
+        }
+    }
+
     private void Awake() {
         //DontDestroyOnLoad(this.gameObject);
     }
