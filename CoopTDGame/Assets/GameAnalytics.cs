46b1a483fb636989d09daedacd33ec6f77206427
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2019-06-17 00:27:47 +0200

    Separated player analytics

--- CoopTDGame/Assets/GameAnalytics.cs
+++ CoopTDGame/Assets/GameAnalytics.cs
@@ -5,99 +5,158 @@ using UnityEngine.Analytics;
 
 public class GameAnalytics : Singleton<GameAnalytics>
 {
-    public int playerDeath, playerRevive;
-    public int playerJump, playerRangedAttack, playerMeleeAttack, playerHeal, playerUltimate, playerRiseTrap, playerRiseTower;
+    #region Player1
+    [System.Serializable]
+    public class Player1 {
+        public int playerDeath, playerRevive;
+        public int playerJump, playerRangedAttack, playerMeleeAttack, playerHeal, playerUltimate, playerRiseTrap, playerRiseTower;
+    }
+    [SerializeField] public Player1 _Player1;
+    #endregion
+
+    #region Player2
+    [System.Serializable]
+    public class Player2 {
+        public int playerDeath, playerRevive;
+        public int playerJump, playerRangedAttack, playerMeleeAttack, playerHeal, playerUltimate, playerRiseTrap, playerRiseTower;
+    }
+    [SerializeField] public Player2 _Player2;
+    #endregion
+
+    
+
+
     public int trapCombinationFire, trapCombinationIce, trapCombinationBlast, towerCombinationFire, towerCombinationIce, towerCombinationBlast;
     public int meleeEnemyCombinationFire, meleeEnemyCombinationIce, meleeEnemyCombinationBlast, rangedEnemyCombinationFire, rangedEnemyCombinationIce, rangedEnemyCombinationBlast, bossEnemyCombinationFire, bossEnemyCombinationIce, bossEnemyCombinationBlast;
     public int enemyDeath;
     public int soulsPickedUp, soulsUsed;
     public int waveReached;
 
-    public void PlayerDeath(int player) {
-        playerDeath++;
-
-        Analytics.CustomEvent("Player Death", new Dictionary<string, object>
+    void TriggerCustomEvent(string eventName, string param1, int _param1, string param2, int _param2) {
+        Analytics.CustomEvent(eventName, new Dictionary<string, object>
         {
-            { "Player", player },
-            { "Deaths", playerDeath }
+            { param1, _param1 },
+            { param2, _param2 }
         });
+    }
+
+    public void PlayerDeath(int player) {
+        if (player == 1) {
+            _Player1.playerDeath++;
+
+            TriggerCustomEvent("Player1 Death", "Player", player, "Deaths", _Player1.playerDeath);
+        }
+        else if (player == 2) {
+            _Player2.playerDeath++;
+
+            TriggerCustomEvent("Player2 Death", "Player", player, "Deaths", _Player2.playerDeath);
+        }
     }
 
     public void PlayerRevive(int player) {
-        playerRevive++;
+        if (player == 1) {
+            _Player1.playerRevive++;
 
-        Analytics.CustomEvent("Player Revive", new Dictionary<string, object>
-        {
-            { "Player", player },
-            { "Deaths", playerRevive }
-        });
+            TriggerCustomEvent("Player1 Revive", "Player", player, "Revives", _Player1.playerRevive);
+        }
+        else if (player == 2) {
+            _Player2.playerRevive++;
+
+            TriggerCustomEvent("Player2 Revive", "Player", player, "Revives", _Player2.playerRevive);
+        }
     }
 
     public void PlayerJump(int player) {
-        playerJump++;
+        if (player == 1) {
+            _Player1.playerJump++;
 
-        Analytics.CustomEvent("Player Jump", new Dictionary<string, object>
-        {
-            { "Player", player },
-            { "Jumps", playerJump }
-        });
+            TriggerCustomEvent("Player1 Jump", "Player", player, "Jumps", _Player1.playerJump);
+        }
+        else if (player == 2) {
+            _Player2.playerJump++;
+
+            TriggerCustomEvent("Player2 Jump", "Player", player, "Jumps", _Player2.playerJump);
+        }
     }
 
     public void PlayerRangedAttack(int player) {
-        playerRangedAttack++;
+        if (player == 1) {
+            _Player1.playerRangedAttack++;
 
-        Analytics.CustomEvent("Player Ranged Attack", new Dictionary<string, object>
-        {
-            { "Player", player },
-            { "Ranged Attacks", playerRangedAttack }
-        });
+            TriggerCustomEvent("Player1 Ranged Attack", "Player", player, "Ranged Attacks", _Player1.playerRangedAttack);
+        }
+        else if (player == 2) {
+            _Player2.playerRangedAttack++;
+
+            TriggerCustomEvent("Player2 Ranged Attack", "Player", player, "Ranged Attacks", _Player2.playerRangedAttack);
+        }
     }
 
     public void PlayerMeleeAttack(int player) {
-        playerMeleeAttack++;
+        if (player == 1) {
+            _Player1.playerMeleeAttack++;
 
-        Analytics.CustomEvent("Player Melee Attack", new Dictionary<string, object>
-        {
-            { "Player", player },
-            { "Melee Attacks", playerMeleeAttack }
-        });
+            TriggerCustomEvent("Player1 Melee Attack", "Player", player, "Melee Attacks", _Player1.playerMeleeAttack);
+        }
+        else if (player == 2) {
+            _Player2.playerMeleeAttack++;
+
+            TriggerCustomEvent("Player2 Melee Attack", "Player", player, "Melee Attacks", _Player2.playerMeleeAttack);
+        }
     }
 
     public void PlayerHeal(int player) {
-        playerHeal++;
+        if (player == 1) {
+            _Player1.playerHeal++;
 
-        Analytics.CustomEvent("Player Heal Ability", new Dictionary<string, object>
-        {
-            { "Player", player },
-            { "Heals", playerHeal }
-        });
+            TriggerCustomEvent("Player1 Heal", "Player", player, "Heals", _Player1.playerHeal);
+        }
+        else if (player == 2) {
+            _Player2.playerHeal++;
+
+            TriggerCustomEvent("Player2 Heal", "Player", player, "Heals", _Player2.playerHeal);
+        }
     }
 
     public void PlayerUltimate(int player) {
-        playerUltimate++;
+        if (player == 1) {
+            _Player1.playerHeal++;
 
-        Analytics.CustomEvent("Player Ultimate Ability", new Dictionary<string, object>
-        {
-            { "Player", player },
-            { "Ultimates", playerUltimate }
-        });
+            TriggerCustomEvent("Player1 Ultimate", "Player", player, "Ultimates", _Player1.playerHeal);
+        }
+        else if (player == 2) {
+            _Player2.playerHeal++;
+
+            TriggerCustomEvent("Player2 Ultimate", "Player", player, "Ultimates", _Player2.playerHeal);
+        }
     }
 
     public void PlayerRise(int player, bool isTower) {
         if (!isTower) {
-            playerRiseTrap++;
+            if (player == 1) {
+                _Player1.playerRiseTrap++;
+
+                TriggerCustomEvent("Player1 Rise Trap", "Player", player, "Rise Traps", _Player1.playerRiseTrap);
+            }
+            else if (player == 2) {
+                _Player2.playerRiseTrap++;
+
+                TriggerCustomEvent("Player2 Rise Trap", "Player", player, "Rise Traps", _Player2.playerRiseTrap);
+            }
         }
         else
         {
-            playerRiseTower++;
-        }
+            if (player == 1) {
+                _Player1.playerRiseTower++;
 
-        Analytics.CustomEvent("Player Rise Ability", new Dictionary<string, object>
-        {
-            { "Player", player },
-            { "Risen Traps", playerRiseTrap },
-            { "Risen Towers", playerRiseTower }
-        });
+                TriggerCustomEvent("Player1 Rise Tower", "Player", player, "Rise Towers", _Player1.playerRiseTower);
+            }
+            else if (player == 2) {
+                _Player2.playerRiseTower++;
+
+                TriggerCustomEvent("Player2 Rise Tower", "Player", player, "Rise Towers", _Player2.playerRiseTower);
+            }
+        }
     }
 
     public void PrefabCombination(Element element, bool isTower) {
@@ -248,15 +307,25 @@ public class GameAnalytics : Singleton<GameAnalytics>
 
     void Reset()
     {
-        playerDeath = 0;
-        playerRevive = 0;
+        _Player1.playerDeath = 0;
+        _Player1.playerRevive = 0;
+        _Player1.playerJump = 0;
+        _Player1.playerRangedAttack = 0;
+        _Player1.playerMeleeAttack = 0;
+        _Player1.playerHeal = 0;
+        _Player1.playerUltimate = 0;
+        _Player1.playerRiseTrap = 0;
+        _Player1.playerRiseTower = 0;
 
-        playerJump = 0;
-        playerRangedAttack = 0;
-        playerMeleeAttack = 0;
-        playerHeal = 0;
-        playerUltimate = 0;
-        playerRiseTrap = 0;
+        _Player2.playerDeath = 0;
+        _Player2.playerRevive = 0;
+        _Player2.playerJump = 0;
+        _Player2.playerRangedAttack = 0;
+        _Player2.playerMeleeAttack = 0;
+        _Player2.playerHeal = 0;
+        _Player2.playerUltimate = 0;
+        _Player2.playerRiseTrap = 0;
+        _Player2.playerRiseTower = 0;
 
         trapCombinationFire = 0;
         trapCombinationIce = 0;
