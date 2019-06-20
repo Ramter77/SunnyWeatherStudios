5e2a8d41344a9977ae411617e5cefe7388ef6702
Ramter77 <34648210+Ramter77@users.noreply.github.com>
2019-06-20 06:12:45 +0200

    Fixed boss weapon colldier & prevent pushing enemies
    Increased stats of Boss
    Setting enemies to kinematic on contact with players

---
+++ CoopTDGame/Assets/PreventPushing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreventPushing : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Player2") {
+            rb.isKinematic = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Player2") {
+            rb.isKinematic = false;
+        }
+    }
+}
